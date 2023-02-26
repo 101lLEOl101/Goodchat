30238f3457e94a6cbb5de47fe1d6acdd272f739f
Anna Gordienko <annagord2006@gmail.com>
2023-02-26 18:11:11 +0300

    модель profile

--- main/GC/models.py
+++ main/GC/models.py
@@ -1,2 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    avatar = models.ImageField(upload_to="images/profile/")
+    about = models.CharField(max_length = 500)
+    country = models.TextField()
+    city = models.TextField()
+    education = models.TextField()
+    company = models.TextField()
+    hobby = models.TextField()
