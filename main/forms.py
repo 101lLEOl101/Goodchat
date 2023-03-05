b1e720345737a3034a1127aba4325d74a8a2d082
Leonid Kuchuk <Lenya.kuchuk@gmail.com>
2023-03-05 14:35:24 +0300

    add brok registration

---
+++ main/forms.py
@@ -0,0 +1,50 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
+
+class RegistrForm(UserCreationForm):
+    username = forms.CharField(label='Логин', widget=forms.TextInput(attrs={
+        'class': 'form-input',
+        'placeholder': 'Логин'
+    }))
+    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={
+        'class': 'form-input',
+        'placeholder': 'Пароль'
+    }))
+    password2 = forms.CharField(label='Повтор пароля', widget=forms.PasswordInput(attrs={
+        'class': 'form-input',
+        'placeholder': 'Повторите пароль'
+    }))
+    email = forms.EmailField(max_length=254, widget=forms.EmailInput(attrs={
+        'class': 'form-input',
+        'placeholder': 'E-mail'
+    }))
+    nickname = forms.CharField(label='Повтор пароля', widget=forms.PasswordInput(attrs={
+        'class': 'form-input',
+        'placeholder': 'Никнейм'
+    }))
+
+    class Meta:
+        model = User
+        fields = ('nickname', 'username', 'email', 'password1', 'password2')
+
+    def clean_password2(self):
+        password1 = self.cleaned_data['password1']
+        password2 = self.cleaned_data['password2']
+
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Пароли не совпадают")
+        return password2
+
+
+class LogForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-input',
+        'placeholder': 'Логин'
+    }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-input',
+        'placeholder': 'Пароль'
+    }))
--- main/views.py
+++ main/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from .forms import RegistrForm
 
 def main_page(request):
  context = {}
  return render(request, "main_page.html", context)
 
+def regist(request):
+ data = {}
+ if request.method == 'POST':
+  form = RegistrForm(request.POST)
+  if form.is_valid():
+   form.save()
+   data['form'] = form
+   data['res'] = "Всё прошло успешно"
+   return render(request, 'reg.html', data)
+ else:
+  form = RegistrForm()
+  data['form'] = form
+ return render(request, 'reg.html', data)
