cb68a6431065224579c264009f82282fa47edab1
Proshunin Nikolay <kolya12911@gmail.com>
2023-03-12 14:39:29 +0300

    Oleg work

--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+from models import Profile
+
 # Create your views here.
+def profile_page(request):
+    return render(request, 'profile_page.html', context={
+        'user': Profile.user,
+        'name': Profile.name,
+        'surname': Profile.surname,
+        'avatar': Profile.avatar,
+        'about': Profile.about,
+        'country': Profile.country,
+        'city': Profile.city,
+        'education': Profile.education,
+        'company': Profile.company,
+        'hobby': Profile.hobby,
+    })
