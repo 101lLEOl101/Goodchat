ea3a37ae4bee7e3774e87f614ba9f8d67476cb8e
Egor Nazaikin <nazaikineg@gmail.com>
2023-03-13 17:13:59 +0300

    Fix Oleg's bugs, add pass profile page, .gitignore update

--- GoodChat/urls.py
+++ GoodChat/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from users import views
-from main import views
+from users import views as users_views
+from main import views as main_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.main_page, name='home'),
-    path('profile/', views.profile_page, name='profile'),
+    path('', main_views.main_page, name='home'),
+    path('profile/<int:id>', users_views.profile_page, name='profile'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,18 +1,8 @@
 from django.shortcuts import render
 
-from models import Profile
+from .models import Profile
 
 # Create your views here.
-def profile_page(request):
-    return render(request, 'profile_page.html', context={
-        'user': Profile.user,
-        'name': Profile.name,
-        'surname': Profile.surname,
-        'avatar': Profile.avatar,
-        'about': Profile.about,
-        'country': Profile.country,
-        'city': Profile.city,
-        'education': Profile.education,
-        'company': Profile.company,
-        'hobby': Profile.hobby,
-    })
+def profile_page(request, id):
+    print(Profile)
+    return render(request, 'profile.html', context={'profile_id': id})
