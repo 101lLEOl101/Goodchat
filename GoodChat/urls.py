b6bc2b8fc56e96399a88d07c4806cc5835b71744
Egor Nazaikin <nazaikineg@gmail.com>
2023-03-15 16:59:28 +0300

    User's avatar is showing on the profile page; media dir

--- GoodChat/urls.py
+++ GoodChat/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from main import views as main_views
 from users import views as users_views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,3 +29,6 @@ urlpatterns = [
     path('login/', users_views.login_page, name='login'),
     path('profile/<int:id>', main_views.profile),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
