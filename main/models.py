e9b28e15cd447bbb279b099a82bc06140ed8172f
Egor Nazaikin <nazaikineg@gmail.com>
2023-03-15 13:37:23 +0300

    Add post model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from users.models import User
 # Create your models here.
+class Post(models.Model):
+    """
+    Model for user text-posts
+    """
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    content = models.TextField()
+    date = models.DateTimeField(auto_now=True)
+    is_edited = models.BooleanField(default=False)
