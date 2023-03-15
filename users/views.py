94140c71b99153d942568c6748d7bdb9779f05c8
Egor Nazaikin <nazaikineg@gmail.com>
2023-03-15 15:56:29 +0300

    Add redirect to the authauthorized users's profile page after authorization

--- users/views.py
+++ users/views.py
@@ -22,7 +22,8 @@ def login_page(request):
 
             if user is not None:
                 login(request, user)
-                return redirect('home')
+                current_user_id = request.user.id
+                return redirect(f'/profile/{current_user_id}')
             else:
                 context['message'] = 'Error: bad login/password'
                 context['form'] = auth_form
