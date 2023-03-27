c28d79ffab5db4b78b4d0df3103034b0fe8d8f30
Leonid Kuchuk <Lenya.kuchuk@gmail.com>
2023-03-27 23:47:20 +0300

    start working with img

--- main/forms.py
+++ main/forms.py
@@ -54,3 +54,8 @@ class PostForm(forms.Form):
         'class': 'text-of-new-post',
         'placeholder': 'Ваш текст'
     }))
+    image=forms.ImageField(widget=forms.ClearableFileInput(attrs={
+        'class': 'add-img-post',
+        'style': 'background-color: #8270F2; width: 100%',
+        'placeholder': 'Ваше изображение'
+    }))
