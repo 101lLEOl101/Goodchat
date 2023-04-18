5a992bcf6612755613b046f87c299ac09f87297c
Egor Nazaikin <nazaikineg@gmail.com>
2023-04-18 19:26:58 +0300

    Add Friend and FriendRequest models

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import User, Profile
+from .models import User
+from .models import Profile
+from .models import Friend
+from .models import FriendRequest
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Profile)
+admin.site.register(Friend)
+admin.site.register(FriendRequest)
