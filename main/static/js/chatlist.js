69f4e1eb001fcf204bfcb8ab0de0025e781ebe7d
Leo <Lenya.kuchuk@gmail.com>
2023-04-19 13:51:31 +0300

    add limit last_message in chatlist

--- main/static/js/chatlist.js
+++ main/static/js/chatlist.js
@@ -3,3 +3,10 @@ mes.classList.remove("side_icon");
 i = mes.querySelector('.hr').querySelector('.icona');
 i.classList.add("now-icona");
 i.classList.remove("icona");
+window.addEventListener('load', function (){
+for(let i = 0; i < last_message.length; i++){
+if(last_message[i].innerHTML.length >= 40){
+last_message[i].innerHTML = last_message[i].innerHTML.slice(0, 40) + "...";
+}
+}
+});
