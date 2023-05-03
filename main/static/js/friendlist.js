d76cda296728b0803f57595f1b27ea3ea158316b
r3ch <davidkakubava@gmail.com>
2023-05-03 22:55:45 +0400

    ожидается фикс для всех элементов

--- main/static/js/friendlist.js
+++ main/static/js/friendlist.js
@@ -4,6 +4,29 @@ i = friend.querySelector('.hr').querySelector('.icona');
 i.classList.add("now-icona");
 i.classList.remove("icona");
 
-btn_h1.addEventListener('click', function() {
-        document.getElementById("req_h1").style.display = "none";
+var button = document.getElementById("btn_h2");
+var button_2 = document.getElementById("btn_h1");
+var title = document.getElementById("req_h1");
+var title_2 = document.getElementById("req_h2");
+var request = document.getElementById("request_item");
+var friends = document.getElementById("friend_item");
+
+button.addEventListener('click', function() {
+        title.style.display = "none";
+        button.style.display = "none";
+        title_2.style.display = "block";
+        button_2.style.display = "block";
+        request.style.display = "flex";
+        friends.style.display = "none";
+        console.log("Кнопка нажата.");
+});
+
+button_2.addEventListener('click', function() {
+        title.style.display = "block";
+        button.style.display = "block";
+        friends.style.display = "flex";
+        title_2.style.display = "none";
+        button_2.style.display = "none";
+        request.style.display = "none";
+        console.log("Кнопка_2 нажата.");
 });
