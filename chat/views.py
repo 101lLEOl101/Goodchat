44c98c732093d1134f481244b534038c513813cf
Leonid Kuchuk <Lenya.kuchuk@gmail.com>
2023-05-06 02:39:35 +0300

    rename function

--- chat/views.py
+++ chat/views.py
@@ -195,18 +195,7 @@ def leave_chat(request, chat_id):
         leave_chat_util(access.user, access.chat)
     return redirect('chat-list')
 
-
 def new_multychat(request):
-    chat = Chat(is_multy=True, name="Untitled")
-    chat.save()
-    access = Access(user=request.user, chat=chat, mode=4)
-    access.save()
-    message = Message(chat=chat, author=request.user,
-                      content="У меня право на первое сообщение на уровне сервера хахаха")
-    message.save()
-    return redirect('chat-list')
-
-def re_new_multychat(request):
     context = {}
     if request.method == 'POST':
         print(request.POST)
