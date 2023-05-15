07f773dd072a77d8b68a2edda58a9a4212214643
r3ch <davidkakubava@gmail.com>
2023-05-15 23:36:42 +0400

    по ходу разборки с запуском много что  добавлено. Добавлена страничка настроек и в индекс дж строчка которая пофиксила редирект на логин и в сеттинг пай убрана строчка которая не дала запустить сервер

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,12 +4,18 @@ import RegistrationPage from '@/views/RegistrationPage'
 import LoginPage from '@/views/LoginPage'
 import ProfilePage from '@/views/ProfilePage'
 import NotFoundPage from '@/views/NotFoundPage'
+import SettingsPage from '@/views/SettingsPage'
 
 const routes = [
   {
     path: '/registration',
     name: 'registration',
     component: RegistrationPage
+  },
+  {
+    path: '/settings',
+    name: 'settings-page',
+    component: SettingsPage
   },
   {
     path: '/login',
@@ -36,7 +42,7 @@ const router = createRouter({
 
 router.beforeEach((to, from, next) => {
   // redirect to login page if not logged in and trying to access a restricted page
-  const publicPages = ['/login'];
+  const publicPages = ['/login', '/registration'];
   const authRequired = !publicPages.includes(to.path);
   const loggedIn = UserManager.getUser();
 
