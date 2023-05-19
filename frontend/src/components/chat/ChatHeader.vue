600ed7551e1744b423258849a0df6d80cdcf8e6e
r3ch <davidkakubava@gmail.com>
2023-05-19 16:06:51 +0400

    добавлены 2 компонента для чата-чат бокс и чат хедер

---
+++ frontend/src/components/chat/ChatHeader.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="ur_friend" >
+        <a class = "chat-avatar-div" href = "{{ chat.info_link }}"><img src="{{chat.avatar.url}}" class="friend-user-pic"></a>
+        <p>{{chat.name}}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {}
+  </script>
+  
+  <style>
+  .ur_friend{
+    height: 7%;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    position: fixed;
+    top:0.7%;
+    left:50%;
+    transform: translateX(-50%) ;
+    z-index:51;
+    width: 200px;
+    font-size:15px;
+}
+.chat-avatar-div{
+    height:100%
+}
+
+.friend-user-pic {
+    height:80%;
+    aspect-ratio: 1/1;
+    margin-right: 20px;
+    box-shadow: 0px 0px 20px #8270F2;
+    border: 1px solid #8270F2;
+    border-radius: 100%;
+}
+  </style>
+  
