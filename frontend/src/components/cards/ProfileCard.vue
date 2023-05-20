8237464b360af6bc0ef950d627dab39752dc4a14
Leonid Kuchuk <Lenya.kuchuk@gmail.com>
2023-05-20 22:03:39 +0300

    fix icon in profile card

--- frontend/src/components/cards/ProfileCard.vue
+++ frontend/src/components/cards/ProfileCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="profile-card">
-    <img :src="user.avatar" class="profile-pic" />
+    <img :src="user.avatar" class="profile-pic image_avatar" />
     <div class="row">
       <div class="left">
         <div class="user-nickname" id="usernick">Name: {{ user.name }} </div>
