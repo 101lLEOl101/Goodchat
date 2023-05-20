9558347ccb56d5af3b3a7e064fc23ebe6bc84a6b
Leonid Kuchuk <Lenya.kuchuk@gmail.com>
2023-05-21 00:39:17 +0300

    add all files

---
+++ frontend/src/components/comments/CommentFeed.vue
@@ -0,0 +1,19 @@
+<template>
+<h1 style="text-align:center; color:white;"> Comments </h1>
+<comment-card /> 
+</template>
+<script>
+import CommentCard from '@/components/comments/CommentCard.vue';
+
+export default {
+  components: {
+    CommentCard,
+  },
+  props: {
+    comments: Array,
+  }
+}
+</script>
+<style>
+
+</style>
