d23c0b36399d14b25c5fd9924c5f3ca6b89ead03
r3ch <davidkakubava@gmail.com>
2023-05-27 00:43:16 +0400

    secret commit

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -14,6 +14,7 @@ import CreatePostPage from '@/views/CreatePostPage'
 import FindFriendPage from '@/views/FindFriendPage'
 import FriendListPage from '@/views/FriendListPage'
 import FriendRequestPage from '@/views/FriendRequestPage'
+import SecretPage from '@/views/SecretPage'
 
 
 const routes = [
@@ -90,6 +91,11 @@ const routes = [
     path: '/friendrequest',
     name: 'friendrequest',
     component: FriendRequestPage,
+  },
+  {
+    path: '/dev',
+    name: 'dev',
+    component: SecretPage,
   }
 ]
 
